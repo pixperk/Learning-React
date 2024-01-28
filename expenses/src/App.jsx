9ab4c00963359152eb527d1f2f096c63c3742c99
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2024-01-28 14:26:42 +0530

    final

--- expenses/src/App.jsx
+++ expenses/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Balance from './components/Balance'
@@ -8,6 +7,7 @@ import AddTransaction from './components/AddTransaction'
 import { GlobalProvider } from './context/GlobalState'
 
 function App() {
+ 
  
 
   return (
