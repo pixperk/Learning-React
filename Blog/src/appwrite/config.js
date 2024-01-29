9c6ab036dc03000cf1a58ed4e29754e2f72b6243
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2024-01-29 22:35:51 +0530

    config

--- Blog/src/appwrite/config.js
+++ Blog/src/appwrite/config.js
@@ -32,6 +32,25 @@ export class Service{
             console.log("Appwrite service :: createPost::error", error);
         }
     }
+
+    async updatePost(slug,{title,  content, featuredImage, status}){
+        try {
+            return await this.databases.updateDocument
+                (conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                }
+                )
+            
+        } catch (error) {
+            console.log("Appwrite service :: updatePost::error", error);
+        }
+    }
 }
 
 const service = new Service()
