e63eb1f6e622340835209553b5b36f6c20c5c765
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2024-01-29 22:14:07 +0530

    authentication

--- Blog/src/appwrite/auth.js
+++ Blog/src/appwrite/auth.js
@@ -14,9 +14,42 @@ export class AuthService {
 
     async createAccount({email, password, name}){
         try {
-            await this.account.create(email, password, name)
+            const userAccount = await this.account.create(ID.unique(),email, password, name)
+            if(userAccount){
+            return this.login({email, password})
+            }
+            else{
+                return userAccount;
+            }
         } catch (error) {
             throw error;
+        }
+    }
+
+    async login ({email, password}){
+        try {
+            return await this.account.createEmailSession(email, password)
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+           return await this.account.get();
+            
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error", error)
+        }
+
+        return null;
+    }
+
+    async logout(){
+        try {
+            return await this.account.deleteSessions()
+        } catch (error) {
+            console.log("Appwrite service :: logout :: error", error);
         }
     }
 }
