21f6cca932b39f35e6ee6468bcb1b89975e957ab
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2024-02-04 19:14:40 +0530

    changes

--- tshirt/src/components/ColorPicker.jsx
+++ tshirt/src/components/ColorPicker.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { SketchPicker } from "react-color";
+import { useSnapshot } from "valtio";
+
+import state from "../store";
 
 const ColorPicker = () => {
+  const snap = useSnapshot(state);
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="absolute left-full ml-3">
+      <SketchPicker
+      color={snap.color}
+      disableAlpha
+      /* presetColors={[
+        "#FFD700", "#FF69B4", "#00CED1", "#FF4500", "#7B68EE",
+        "#00FF00", "#800080", "#4682B4", "#FF6347", "#8A2BE2",
+        
+      ]} */
+      onChange={(color)=>state.color = color.hex}
+      />
+      </div>
+  );
+};
 
-export default ColorPicker
+export default ColorPicker;
